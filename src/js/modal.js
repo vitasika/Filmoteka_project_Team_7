35492f9b7f7418cedca58198258a39f534d1535c
eduard-ops <epustynnik@gmail.com>
2022-04-12 21:34:37 +0300

    скрипт на функионал

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,20 +2,40 @@ const modalEl = document.querySelector('[data-modal]');
 
 const buttonClose = document.querySelector('.modal__close-button');
 
-const elem = document.querySelector('.cards-back-text');
+const cardList = document.querySelector('.card-set');
 
-buttonClose.addEventListener('click', toggleModal);
+buttonClose.addEventListener('click', onClickCloseButton);
 
-elem.addEventListener('click', toggleModal);
+cardList.addEventListener('click', onClickCard);
 
-function toggleModal() {
-  modalEl.classList.toggle('is-hidden');
+function onClickCard(e) {
+  if (!e.target.classList.contains('description_films')) {
+    return;
+  }
+  document.body.style.overflow = 'hidden';
+  modalEl.classList.remove('is-hidden');
   window.addEventListener('keydown', OnCloseModalEsc);
+  modalEl.addEventListener('click', onClickBackdrop);
+}
+
+function onClickCloseButton() {
+  modalEl.classList.add('is-hidden');
+  removeOverflow();
+}
+
+function onClickBackdrop(e) {
+  e.target.classList.contains('backdrop') ? modalEl.classList.add('is-hidden') : false;
+  removeOverflow();
 }
 
 function OnCloseModalEsc(e) {
   if (e.code === 'Escape') {
     modalEl.classList.add('is-hidden');
+    removeOverflow();
     window.removeEventListener('keydown', OnCloseModalEsc);
   }
+}
+
+function removeOverflow() {
+  document.body.style.overflow = '';
 }
