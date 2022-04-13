80f6a52540edf38f777581785d50559ae0123716
Mark <markyatsuk12.04.16@gmail.com>
2022-04-13 18:23:25 +0200

    features/team-modal

--- src/index.js
+++ src/index.js
@@ -2,11 +2,4 @@ import './sass/main.scss';
 import './js/header';
 import './js/pagination';
 import './js/to_top';
-import {
-  teamLink,
-  teamModalEl,
-  closeBtn,
-  openModal,
-  closeModal,
-  onLinkClick,
-} from './js/team-modal/team-modal.js';
+import './js/team-modal/team-modal';
--- src/js/team-modal/team-modal.js
+++ src/js/team-modal/team-modal.js
@@ -1,5 +1,5 @@
 const teamLink = document.querySelector('a[data-action="open-modal"]');
-const teamModalEl = document.querySelector('[data-modal]');
+const teamModalEl = document.querySelector('[data-modal-backdrop]');
 const closeBtn = document.querySelector('.cross');
 
 const openModal = teamLink.addEventListener("click", onLinkClick);
@@ -10,4 +10,4 @@ function onLinkClick(e){
     teamModalEl.classList.toggle('is-hidden');
 }
 
-export { teamLink, teamModalEl, closeBtn, openModal, closeModal, onLinkClick };
+// export { teamLink, teamModalEl, closeBtn, openModal, closeModal, onLinkClick };
