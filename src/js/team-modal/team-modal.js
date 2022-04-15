8d3aa7611687679064f2f7eceb3c4dcc17d29609
Mark <markyatsuk12.04.16@gmail.com>
2022-04-16 00:51:14 +0200

    features team-modal
    модернизировал модалку

--- src/js/team-modal/team-modal.js
+++ src/js/team-modal/team-modal.js
@@ -1,7 +1,3 @@
-// const teamLink = document.querySelector('a[data-action="open-modal"]');
-// const teamModalEl = document.querySelector('[data-modal-backdrop]');
-// const closeBtn = document.querySelector('.cross');
-
 import { refs } from '../refs/refs';
 
 refs.teamLink.addEventListener("click", onLinkClick);
