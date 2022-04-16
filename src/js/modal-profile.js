9cdb4ba7acb0113d61b42604fa5cad32bd123036
Maxim Moskalik <moscowalik95@gmail.com>
2022-04-16 23:01:28 +0300

    add users cabinet

--- src/js/modal-profile.js
+++ src/js/modal-profile.js
@@ -3,12 +3,13 @@ const refs = {
   closeModalBtn: document.querySelector('[modal-profile-close]'),
   modal: document.querySelector('[modal-profile-data]'),
   registerClose: document.querySelector('[modal-profile-register]'),
+  closeUserBtn: document.querySelector('[modal-user-close]'),
 };
 
 refs.openModalBtn.addEventListener('click', toggleModal);
 refs.closeModalBtn.addEventListener('click', toggleModal);
 refs.registerClose.addEventListener('click', toggleModal);
-
+refs.closeUserBtn.addEventListener('click', toggleModal);
 function toggleModal(e) {
   e.preventDefault();
   refs.modal.classList.toggle('profile-hidden');
