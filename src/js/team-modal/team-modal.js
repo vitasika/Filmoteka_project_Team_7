6a271b0f239a233e9b196cec1939b4ff707233a5
Mark <markyatsuk12.04.16@gmail.com>
2022-04-17 19:15:36 +0200

    features team-modal

--- src/js/team-modal/team-modal.js
+++ src/js/team-modal/team-modal.js
@@ -1,9 +1,47 @@
 import { refs } from '../refs/refs';
+import { getScrollBarWidth } from '../modal';
 
-refs.teamLink.addEventListener("click", onLinkClick);
-refs.closeBtn.addEventListener("click", onLinkClick);
+refs.teamLink.addEventListener("click", modalTeamOpen);
+refs.closeBtn.addEventListener("click", modalTeamClose);
+refs.teamModalEl.addEventListener("click", onClickTeamBackdrop);
 
-function onLinkClick(e){
+function modalTeamOpen(e){
     e.preventDefault();
-    refs.teamModalEl.classList.toggle('is-hidden');
+    refs.teamModalEl.classList.remove('is-hidden');
+    document.body.style.paddingRight = `${getScrollBarWidth()}px`;
+    document.body.style.overflow = 'hidden';
+    window.addEventListener('keydown', OnCloseTeamModalEsc);
 }
+
+function modalTeamClose(e){
+    refs.teamModalEl.classList.add('is-hidden');
+    addTransitionTeam();
+}
+
+function onClickTeamBackdrop(e){
+    if (e.target.classList.contains('backdrop--team')) {
+        refs.teamModalEl.classList.add('is-hidden');
+      }
+      addTransitionTeam();
+}
+
+function showScrollTeam(e) {
+    console.log(e.propertyName);
+    if (e.propertyName == 'opacity') {
+      document.body.style.overflow = 'visible';
+      document.body.style.paddingRight = ' ';
+      refs.teamModalEl.removeEventListener('transitionend', showScrollTeam);
+    }
+  }
+  
+  function addTransitionTeam() {
+    refs.teamModalEl.addEventListener('transitionend', showScrollTeam);
+  }
+
+ function OnCloseTeamModalEsc(e){
+    if (e.code === 'Escape') {
+        refs.teamModalEl.classList.add('is-hidden');
+        window.removeEventListener('keydown', OnCloseTeamModalEsc);
+        addTransitionTeam();
+      }
+ }
